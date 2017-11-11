f683e67adc5124a8fd7af736c1c741713eb2dadf
Robert Skarżycki <robert.skarzycki@gmail.com>
2017-11-11 11:48:15 +0100

    Klasa Customer w kontrakcie i dodanie referencji

--- src/service.contract/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace service.contract
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/service.contract/Customer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace service.contract
+{
+    public class Customer
+    {
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+    }
+}
