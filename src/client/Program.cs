0fe32424cdfd3a2a4f82a61bad15cc5c49feb43c
Robert Skarżycki <robert.skarzycki@gmail.com>
2017-11-11 16:30:54 +0100

    Referencja do kontraktu przez nuget, klient deserializuje obiekt

--- src/client/Program.cs
+++ src/client/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using service.contract;
 
 namespace client
 {
@@ -6,7 +9,16 @@ namespace client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var client = new HttpClient())
+            {
+                var rawCustomer = client.GetStringAsync("http://localhost:5000/api/Customers/1").Result;
+
+                Console.WriteLine(rawCustomer);
+
+                var customer = JsonConvert.DeserializeObject<Customer>(rawCustomer);
+
+                Console.WriteLine($"Imię: {customer.FirstName}. Nazwisko: {customer.LastName}");
+            }
         }
     }
 }
